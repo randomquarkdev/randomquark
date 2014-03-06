d2e8e61e23d0dc735b628b062fc23d18eabba60a
root <root@ubuntu.(none)>
2014-03-06 21:13:20 +0000

    changes

--- src/bitcoinrpc.cpp
+++ src/bitcoinrpc.cpp
@@ -43,7 +43,7 @@ static boost::thread_group* rpc_worker_group = NULL;
 
 static inline unsigned short GetDefaultRPCPort()
 {
-    return GetBoolArg("-testnet", false) ? 9009 : 9008;
+    return GetBoolArg("-testnet", false) ? 9119 : 9118;
 }
 
 Object JSONRPCError(int code, const string& message)
--- src/protocol.h
+++ src/protocol.h
@@ -18,7 +18,7 @@
 extern bool fTestNet;
 static inline unsigned short GetDefaultPort(const bool testnet = fTestNet)
 {
-    return testnet ? 8009 : 8008;
+    return testnet ? 8119 : 8118;
 }
 
 
